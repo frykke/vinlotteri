bdfe2ed0ef7554e8b4cd0ac2500935df9009bfc2
Mats Fryklund <mats.fryklund@futurpension.se>
2020-04-15 10:53:03 +0200

    Referensproblem?

--- src/app/die/die.component.ts
+++ src/app/die/die.component.ts
@@ -11,9 +11,9 @@ export class DieComponent implements OnInit, OnChanges {
 
   @Input() value = 6;
   @Input() rolling: boolean;
-  private audio =new Audio();
+  private audio = new Audio();
   ngOnInit() {
-    this.audio.src = '../../../assets/16509_1460655272.mp3';
+    this.audio.src = '../../../16509_1460655272.mp3';
     this.audio.load();
   }
   ngOnChanges(changes: SimpleChanges): void {

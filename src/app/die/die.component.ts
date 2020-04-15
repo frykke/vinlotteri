de78d50e90f9b81132987742bc22021b84e6e6f9
Mats Fryklund <mats.fryklund@futurpension.se>
2020-04-15 10:05:48 +0200

    Med ljud

--- src/app/die/die.component.ts
+++ src/app/die/die.component.ts
@@ -1,16 +1,27 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-die',
   templateUrl: './die.component.html',
   styleUrls: ['./die.component.scss']
 })
-export class DieComponent implements OnInit {
+export class DieComponent implements OnInit, OnChanges {
 
   constructor() { }
+
   @Input() value = 6;
   @Input() rolling: boolean;
+  private audio =new Audio();
   ngOnInit() {
+    this.audio.src = '../../../assets/16509_1460655272.mp3';
+    this.audio.load();
   }
-
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.rolling && !changes.rolling.firstChange && changes.rolling.currentValue) {
+      this.rollSound();
+    }
+  }
+  rollSound() {
+    this.audio.play();
+  }
 }
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -29,7 +29,7 @@ export class MainComponent implements OnInit {
       this.rolling = false;
       this.stake = '';
       this.gameService.roll(stake);
-      }, 2000);
+      }, 4000);
   }
   onClickLower() {
     this.roll('lower');
